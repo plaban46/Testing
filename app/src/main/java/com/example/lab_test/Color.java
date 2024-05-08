05f5f655acfbb553590e9b6a8256a11b94a0c5fe
Plaban <turjoyplaban09@gmail.com>
2024-05-09 03:36:40 +0600

    CCCC

--- app/src/main/java/com/example/lab_test/Color.java
+++
@@ -1,6 +0,0 @@
-package com.example.lab_test;
-
-public abstract class Color {
-    public String name;
-    public abstract String Showcolor();
-}
--- app/src/main/java/com/example/lab_test/Red.java
+++
@@ -1,9 +0,0 @@
-package com.example.lab_test;
-
-public class Red extends Color{
-    @Override
-    public String Showcolor() {
-        name = "Red";
-        return name;
-    }
-}
